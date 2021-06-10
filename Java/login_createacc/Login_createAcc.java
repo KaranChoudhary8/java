99dac4eea211695330213ca20238d3fde2171962
KaranChoudhary8 <80000771+KaranChoudhary8@users.noreply.github.com>
2021-06-10 17:48:09 +0530

    Add files via upload

---
+++ Java/login_createacc/Login_createAcc.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login_createacc;
+
+/**
+ *
+ * @author karan
+ */
+public class Login_createAcc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginUI login = new LoginUI();
+    }
+    
+}
